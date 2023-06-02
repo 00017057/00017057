dae5a2ced34ff1cf6a4c7fadd27ae1c65c123490
00017057 <118054728+00017057@users.noreply.github.com>
2023-06-02 15:51:11 +0500

    'create' and 'read' operations added

--- app.js
+++ app.js
@@ -1,12 +1,65 @@
+
+// third party libraries
 const express = require('express')
 const app = express()
+
+// node libraries
+const fs = require('fs')
 const PORT = 8000
 
 app.set('view engine', 'pug')
+app.use('/static', express.static('public')) // assets
+app.use(express.urlencoded({ extended: false }))
 
 // http://localhost:8000
 app.get('/', (req, res) => {
-    res.render('home')
+    fs.readFile('./data/shpitems.json', (err, data) => {
+        if (err) throw err
+
+        const shpitems = JSON.parse(data)
+
+        res.render('home', { shpitems: shpitems })
+    })
+})
+
+app.post('/add', (req, res) => {
+    const formData = req.body
+
+    if (formData.shoppingitem.trim() == '') {
+        fs.readFile('./data/shpitems.json', (err, data) => {
+            if (err) throw err
+
+            const shpitems = JSON.parse(data)
+
+            res.render('home', { error: true, shpitems: shpitems })
+        })
+    } else {
+        fs.readFile('./data/shpitems.json', (err, data) => {
+            if (err) throw err
+
+            const shpitems = JSON.parse(data)
+
+            const shpitem = {
+                id: id(),
+                description: formData.shoppingitem,
+                done: false
+            }
+
+            shpitems.push(shpitem)
+
+            fs.writeFile('./data/shpitems.json', JSON.stringify(shpitems), (err) => {
+                if (err) throw err
+
+                fs.readFile('./data/shpitems.json', (err, data) => {
+                    if (err) throw err
+
+                    const shpitems = JSON.parse(data)
+
+                    res.render('home', { success: true, shpitems: shpitems })
+                })
+            })
+        })
+    }
 })
 
 app.listen(PORT, (err) => {
@@ -14,3 +67,8 @@ app.listen(PORT, (err) => {
 
     console.log(`This app is running on port ${ PORT }`)
 })
+
+
+function id () {
+    return '_' + Math.random().toString(36).substr(2, 9);
+  }
